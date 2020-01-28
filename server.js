1e452a6076fdf459231a3185f865b7ba7b0dce66
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2020-01-28 11:09:11 -0500

    first build passed, but with unintended problems

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 //Connect Database
 connectDB();
 
-//Init Middleware 
-app.use(express.json({extended: false}));
-
-app.get('/', (req, res) => res.json({ msg: 'I will succeed, at building this Appointment Scheduling API!'}));
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/appointments', require('./routes/appointments'));
 app.use('/api/auth', require('./routes/auth'));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set Static Folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
-const PORT = process.env.PORT || 7676
+const PORT = process.env.PORT || 7676;
 
 app.listen(PORT, () => console.log(`App Server Online on Port: ${PORT}`));
