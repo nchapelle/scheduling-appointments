9e65541f4bd0e068ea4c68caae6c66bd2733ed57
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2020-02-13 20:28:58 -0500

    notes added

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
 
+// function for making sure user is logged in before rendering page
+
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const authContext = useContext(AuthContext);
   const { isAuthenticated, loading } = authContext;
